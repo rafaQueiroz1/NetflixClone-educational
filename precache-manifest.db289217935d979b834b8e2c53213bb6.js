d66d727feba80c5f356a39071649de9c1d9986bb
Rafael Queiroz <rqueiroz3722@gmail.com>
2023-09-12 13:11:19 -0300

    Updates

---
+++ precache-manifest.db289217935d979b834b8e2c53213bb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e96efef3ad825a827d26",
+    "url": "/NetflixClone-educational/static/css/main.aa5cc1e8.chunk.css"
+  },
+  {
+    "revision": "e96efef3ad825a827d26",
+    "url": "/NetflixClone-educational/static/js/main.e96efef3.chunk.js"
+  },
+  {
+    "revision": "45e8e94d18600178d1fa",
+    "url": "/NetflixClone-educational/static/js/1.45e8e94d.chunk.js"
+  },
+  {
+    "revision": "c757257d48550e2750f3",
+    "url": "/NetflixClone-educational/static/js/runtime~main.c757257d.js"
+  },
+  {
+    "revision": "5a520c758e8f6b24e15fcd3d1c58c0f3",
+    "url": "/NetflixClone-educational/index.html"
+  }
+];
